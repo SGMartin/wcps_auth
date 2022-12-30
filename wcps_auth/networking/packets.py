c115e230cbe0708d27b741b3140a12159241bace
Santiago Garcia <s.g.martin94@gmail.com>
2022-12-31 00:16:27 +0100

    Add basic launcher packet stub

---
+++ wcps_auth/networking/packets.py
@@ -0,0 +1,16 @@
+from wcps_core.packets import OutPacket
+
+class ClientXorKeys:
+    Send = 0x96
+    Recieve = 0xC3
+
+class PacketList:
+    Launcher = 0x1010
+    ServerList = 0x1100     
+    NickName = 0x1101
+    Test  = 0x1102
+
+class Launcher(OutPacket):
+    def __init__(self):
+        super().__init__(packet_id=PacketList.Launcher, xor_key=ClientXorKeys.Send)
+        self.fill(0, 7)
